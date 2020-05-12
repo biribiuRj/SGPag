2d991408640f8f402ea488a47c089230cf729a04
Joao Rodrigues <joao.biribiu@gmail.com>
2020-05-12 09:23:26 -0300

    Adicionar arquivos de projeto.

---
+++ SGPag.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SGPag.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ SGPag.Repositorio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SGPag.Repositorio
+{
+    public class Class1
+    {
+    }
+}
